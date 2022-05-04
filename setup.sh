039882e1506628b62ebbfe77d7c0085be711bfeb
Wouter de Bruijn <wouter@debruijnonline.nl>
2022-05-04 16:00:04 +0200

    Bootloader verbosity, toolchain support for MacOS (#2)
    * Add toolchains to source
    
    * Added vendored toolchain support for both MacOS and Linux
    
    * Made bootloader more verbose and reliable

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-VERSION=x86_64-elf-7.3.0-Linux-x86_64
-wget http://newos.org/toolchains/${VERSION}.tar.xz
+
+if [[ "$OSTYPE" == *"darwin"* ]]; then
+    echo "Detected MacOS"
+    VERSION=x86_64-elf-7.3.0-Darwin-x86_64
+else
+    echo "Detected Linux OS"
+    VERSION=x86_64-elf-7.3.0-Linux-x86_64
+fi
+
 tar xf ${VERSION}.tar.xz
-rm ${VERSION}.tar.xz
+
 echo export PATH=$(pwd)/${VERSION}/bin/:$PATH >> .settings
 echo export CROSS_COMPILE=x86_64-elf- >> .settings
